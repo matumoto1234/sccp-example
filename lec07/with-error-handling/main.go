40c2813443607af03b32f2108325acafca384c15
matumoto1234 <matumotosizuoka@gmail.com>
2023-12-07 16:39:31 +0900

    server port 8000 -> 8080

--- lec07/with-error-handling/main.go
+++ lec07/with-error-handling/main.go
@@ -42,5 +42,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
